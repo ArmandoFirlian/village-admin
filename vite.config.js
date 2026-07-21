2e4a76646a2e6140f4cab8379680f0d19f18a4e1
Armando <juliantogusse70@gmail.com>
2026-07-21 20:49:47 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,28 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    root: '.',
-    publicDir: 'public',
-    build: {
-        outDir: 'dist',
-        emptyOutDir: true,
-        minify: 'esbuild',
-        sourcemap: false,
-        terserOptions: {
-            compress: {
-                drop_console: false,
-            },
-        },
+  root: '.',
+  publicDir: 'public',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    minify: 'esbuild',
+    sourcemap: false,
+    // Opsi Multi-Page Application (MPA)
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        wisata: resolve(__dirname, 'wisata-umkm.html'),
+      },
     },
-    server: {
-        port: 3000,
-        open: true,
-    },
+  },
+  esbuild: {
+    // Jika ingin menghapus console.log saat production, ubah drop_console ke true
+    // drop: ['console'], 
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
 });
